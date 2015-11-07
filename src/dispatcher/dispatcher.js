d1a17cfd4f42136c2f06df66ca4696489c328331
zpenka <zpenka@gmail.com>
2015-11-07 18:22:26 -0500

    Create Dispatcher, Actions

---
+++ src/dispatcher/dispatcher.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Tempest Live Image Filter
+// @version 0.0.1
+// @author Zack Penka (following React.js Essentials, Artemij Fedosejev)
+//
+
+import {Dispatcher} from 'flux';
+export default new Dispatcher();
