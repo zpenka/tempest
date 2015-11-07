d06fc488746e7e82ecdd214ae0f5d81ed34fbb95
zpenka <zpenka@gmail.com>
2015-11-06 23:16:08 -0500

    Create React App
    Additional tasks complete: Add postcss wrapper function, add first
    components, refine gulpfile- add minification process, update
    dependencies

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@
 // @version 0.0.1
 // @author Zack Penka (following React.js Essentials, Artemij Fedosejev)
 //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tempest from './components/tempest.js';
+
+// Get mount point
+const el = document.getElementById('tempest');
+
+// Mount react to the document
+ReactDOM.render(<Tempest />, el);
