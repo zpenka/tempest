f631b0da009199cdf9311e4196f1c2816f0c3c49
zpenka <zpenka@gmail.com>
2015-11-07 14:59:55 -0500

    Add app utilities folder

---
+++ src/utils/tweet-utils.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Tempest Live Image Filter
+// @version 0.0.1
+// @author Zack Penka (following React.js Essentials, Artemij Fedosejev)
+//
+
+const Utils = {
+  getListOfTweetIDs (tweets) {
+    return Object.keys(tweets);
+  }
+}
+
+export default Utils;
